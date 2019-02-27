793f3086d43d5d484e6c8369041e0cd24c0b7a7c
ksepheu <47608242+ksepheu@users.noreply.github.com>
2019-02-27 09:54:57 +0200

    Added MySql Database Class + Login Form

--- SchemeData/Login.cs
+++ SchemeData/Login.cs
@@ -9,16 +9,11 @@ using System.Windows.Forms;
 
 namespace SchemeData
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-          
         }
     }
 }
---
+++ SchemeData/orgDB.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Odbc;
+using System.Net;
+using System.Windows.Forms;
+using System.Web;
+using System.Net.NetworkInformation;
+
+namespace SchemeData
+{
+    class orgDB
+    {
+        public orgDB()
+        {
+            odbconn = new OdbcConnection("Driver={MySQL ODBC 3.51 Driver};Server=154.0.172.180;Port=3306;Database=seadsenp_orgData; User=seadsenp_emp;Password=dev101;Option=3");
+            odbcmd = new OdbcCommand();
+            odbcmd = odbconn.CreateCommand();
+            odbcDA = new OdbcDataAdapter();
+        }
+        OdbcConnection odbconn;
+        OdbcCommand odbcmd;
+        OdbcDataAdapter odbcDA;
+        public DataSet retDB(String cmdText, String mem)
+        {
+            DataSet dsTemp;
+            odbcmd.CommandType = CommandType.Text;
+            odbcmd.CommandText = cmdText;
+            odbcDA.SelectCommand = odbcmd;                         
+            dsTemp = new DataSet();
+            dsTemp.Clear();
+            odbcDA.Fill(dsTemp, mem);
+            return dsTemp;
+        }
+
+        public void altDB(String cmdText)
+        {
+            //int noR = 0;
+            odbcmd.CommandType = CommandType.Text;
+            odbcmd.CommandText = cmdText;         
+            if (odbcmd.Connection.State == ConnectionState.Closed)
+            {
+                odbcmd.Connection.Open();
+            }
+            odbcmd.ExecuteNonQuery();
+        }
+
+        public bool CheckForInternetConnection()
+        {
+            try
+            {
+                //DataSet dsTemp;
+                //string cmdText, mem;
+                //cmdText = "SHOW TABLES LIKE 'Users'";
+                //mem = "ConnectionTest";
+                //odbcmd.CommandType = CommandType.Text;
+                //odbcmd.CommandText = cmdText;
+                //odbcDA.SelectCommand = odbcmd;
+                //dsTemp = new DataSet();
+                //odbcDA.Fill(dsTemp, mem);
+                //return true;
+
+                string nameOrAddress = "8.8.4.4";
+                bool pingable = false;
+                Ping pinger = new Ping();
+                try
+                {
+                    PingReply reply = pinger.Send(nameOrAddress);
+                    pingable = reply.Status == IPStatus.Success;
+                }
+                catch (PingException)
+                {
+                    // Discard PingExceptions and return false;
+                    return false;
+                }
+                return pingable;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
